eb75310789511eca42c1b413ce26c361612a8888
Glen Maddern <glen@cloudflare.com>
2023-04-20 17:57:27 +1000

    sketching out 4 bases and a quick readme

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
+# invlex — Inverse Lexicographic Encoding
+
+For encoding numbers so that higher numbers appear first, sorted lexicographically (i.e. in a cloud storage bucket that only has ascending LIST operations), in as few bytes as possible.
+
+## Usage
+
+```sh
+npm install --save-dev invlex
+pnpm add -D invlex
+```
+
+```ts
+import invlex from 'invlex'
+
+invlex.encode(0) // "zf"
+invlex.encode(1) // "ze"
+invlex.encode(15) // "z0"
+invlex.encode(16) // "yef"
+invlex.encode(1959) // "x858"
+invlex.encode(49584741) // "td0b659a"
+invlex.encode(350371465044) // "qae6c3bb8ab"
+invlex.encode(84296151321634) // "ob35545d833dd"
+```
+
+```ts
+import { base32 as invlex } from 'invlex'
+
+invlex.encode(0) // "zv"
+invlex.encode(1) // "zu"
+invlex.encode(15) // "zg"
+invlex.encode(16) // "zf"
+invlex.encode(1959) // "xu2o"
+invlex.encode(49584741) // "uugmpcq"
+invlex.encode(350371465044) // "slpm3ne5b"
+invlex.encode(84296151321634) // "qtjal2tgcut"
+```
+
+```ts
+import { base62 as invlex } from 'invlex'
+
+invlex.encode(0) // "zz"
+invlex.encode(1) // "zy"
+invlex.encode(15) // "zk"
+invlex.encode(16) // "zj"
+invlex.encode(1959) // "yUO"
+invlex.encode(49584741) // "vwdwk6"
+invlex.encode(350371465044) // "ttpYL2Pz"
+invlex.encode(84296151321634) // "sc3uyZfif"
+```
+
+## Format
+
+Numbers are converted to a hex string (or base-32 or base-64 string, see below), then inverted (so "001" becomes "ffe"), then prefixed with a single letter that encodes the length, starting at 'z' and moving up the alphabet, meaning that longer (larger) numbers appear first when sorted lexicographically.
+
+This uses a lot less bytes than a naive approach (`Number.MAX_SAFE_INTEGER - n`, for example), and by converting to hex/base32/64, makes the IDs look opaque enough to not look obviously "wrong"
+
+
+## Development
+
 ```sh
 pnpm install
 pnpm tsx index.ts
--- index.ts
+++ index.ts
@@ -1,48 +1,81 @@
 import assert from 'node:assert'
 
+const INVERSE_BASE = {
+  36: 'zyxwvutsrqponmlkjihgfedcba9876543210',
+  62: 'zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA9876543210',
+}
+
+const toBase = (base: keyof typeof INVERSE_BASE) => (num: number) => {
+  let result = ''
+  do {
+    result = INVERSE_BASE[base][num % base] + result
+    num = Math.floor(num / base)
+  } while (num > 0)
+  return result
+}
+
+const fromBase = (base: keyof typeof INVERSE_BASE) => (str: string) => {
+  let num = 0
+  for (let i = 0; i < str.length; i++) {
+    num = num * base + INVERSE_BASE[base].indexOf(str.charAt(i))
+  }
+  return num
+}
+
 const Z_ASCII = 122
-const BASE = 32
+for (const BASE of [16, 32, 36, 62]) {
+  function toInvLex(n: number) {
+    if (n > Number.MAX_SAFE_INTEGER)
+      throw new Error(`9 million billion isn't enough for you?`)
 
-function toInvLex(n: number) {
-  if (n > Number.MAX_SAFE_INTEGER)
-    throw new Error(`9 million billion isn't enough for you?`)
-  const digits = n.toString(BASE).length
-  const inverse = BASE ** digits - n - 1
-  const hex = inverse.toString(BASE).padStart(digits, '0')
-  const prefix = String.fromCharCode(Z_ASCII - (digits - 1))
-  // console.log({n, digits, inverse, hex, prefix})
-  return prefix + hex
-}
+    let encoded
+    if (BASE === 62 || BASE === 36) {
+      encoded = toBase(BASE)(n)
+    } else {
+      const digits = n.toString(BASE).length
+      const inverse = BASE ** digits - n - 1
+      encoded = inverse.toString(BASE).padStart(digits, '0')
+    }
 
-function fromInvLex(str: string) {
-  const [prefix, ...hex] = str
-  const digits = Z_ASCII - prefix.charCodeAt(0) + 1
-  // console.log({str, prefix, hex, digits})
-  if (hex.length !== digits)
-    throw new Error(
-      `Malformed InvLex string, expected ${digits} digit(s) for '${str}', got ${hex.length}.`
+    const prefix = String.fromCharCode(Z_ASCII - (encoded.length - 1))
+    // console.log({ n, digits: encoded.length, encoded, prefix })
+    return prefix + encoded
+  }
+
+  function fromInvLex(str: string) {
+    const [prefix, ...hex] = str
+    const digits = Z_ASCII - prefix.charCodeAt(0) + 1
+    // console.log({str, prefix, hex, digits})
+    if (hex.length !== digits)
+      throw new Error(
+        `Malformed InvLex string, expected ${digits} digit(s) for '${str}', got ${hex.length}.`
+      )
+    return BASE in INVERSE_BASE
+      ? fromBase(BASE as keyof typeof INVERSE_BASE)(hex.join(''))
+      : BASE ** digits - parseInt(hex.join(''), BASE) - 1
+  }
+
+  // Mostly small, some huge numbers
+  const random = () =>
+    Math.round(
+      Math.random() * 20 + Math.random() ** 50 * Number.MAX_SAFE_INTEGER
     )
-  return BASE ** digits - parseInt(hex.join(''), BASE) - 1
+
+  const numbers = new Array(200).fill(null).map((_, i) => i) //Array.from(new Set(new Array(2000).fill(null).map(random))) // unique randoms
+  console.log(numbers)
+
+  const names = numbers.map(toInvLex)
+  console.log(names)
+
+  const sorted_numerically = [...numbers].sort((a, b) => b - a)
+  console.log(sorted_numerically) // inverse numeric sort
+  const sorted_lexicographically = [...names].sort()
+  console.log(sorted_lexicographically)
+
+  assert.deepEqual(sorted_lexicographically, sorted_numerically.map(toInvLex))
+
+  // Show that it's reversible
+  numbers.forEach((n, i) =>
+    console.log(`${n} => ${names[i]} => ${fromInvLex(names[i])}`)
+  )
 }
-
-// Mostly small, some huge numbers
-const random = () =>
-  Math.round(Math.random() * 20 + Math.random() ** 50 * Number.MAX_SAFE_INTEGER)
-
-const numbers = Array.from(new Set(new Array(20).fill(null).map(random))) // unique randoms
-console.log(numbers)
-
-const names = numbers.map(toInvLex)
-console.log(names)
-
-const sorted_numerically = [...numbers].sort((a, b) => b - a);
-console.log(sorted_numerically) // inverse numeric sort
-const sorted_lexicographically = [...names].sort();
-console.log(sorted_lexicographically)
-
-assert.deepEqual(sorted_lexicographically, sorted_numerically.map(toInvLex))
-
-// Show that it's reversible
-numbers.forEach((n, i) =>
-  console.log(`${n} => ${names[i]} => ${fromInvLex(names[i])}`)
-)
